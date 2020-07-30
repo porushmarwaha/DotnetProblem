f4e265fba031036e5380c6fca5c7efa643dea638
porushmarwaha <porushmarwaha@gmail.com>
2020-07-30 16:47:22 +0530

    First Code Implemented to  problem6

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleUI
 {
@@ -6,7 +7,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var decimalList = new List<double>();
+            String[] test = new String[2];
+            string resultString = "";
+            double sum = 0;
+            var stringList = new List<String>();
+            var lunaList = new List<string>(){
+                {"Candy 24.00"},
+                {"Shoes 100.00"},
+                {"MiniDragon 50.4"}
+            };
+            lunaList.ForEach(element =>{ 
+                test = element.Split(' ');
+                decimalList.Add(Convert.ToDouble(test[1]));
+                stringList.Add(test[0]);
+            });
+            decimalList.ForEach(element => sum += element);
+            stringList.ForEach(element => resultString += element +" ");
+            Console.WriteLine($"Sum - {sum}");
+            Console.WriteLine($"Result String - {resultString.Trim()}" );
         }
     }
 }
---
+++ StringLibrary/AbstractSplitClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringLibrary
+{
+    public abstract class AbstractSplitClass
+    {
+        
+    }
+}
